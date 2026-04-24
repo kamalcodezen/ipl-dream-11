93e02d334e5aabcf66e04e5cf5196ced15d002b0
kamalcodezen <kamaluddin7908@gmail.com>
2026-04-24 19:33:17 +0530

    banner structure create

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Banner.css";
+const Banner = () => {
+  return (
+    <section className=" container">
+      <div className="banner-container  bg-purple-950 min-h-[60vh]">
+        
+      </div>
+    </section>
+  );
+};
+
+export default Banner;
