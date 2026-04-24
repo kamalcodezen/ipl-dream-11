12c3aa5254f4345d2c3584ac1e199faa7e602dec
kamalcodezen <kamaluddin7908@gmail.com>
2026-04-24 11:22:58 +0530

    using hook useEffect  and navLink data fetch

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,10 @@ import { Suspense } from "react";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 
-const navaLinkFetch = async () => {
-  const res = await fetch("/public/navbarLinkData.json");
-  return res.json();
-};
-
 function App() {
-  const navLinkPromise = navaLinkFetch();
   return (
     <>
-      <Suspense
-        fallback={<span className="loading loading-dots loading-lg"></span>}
-      >
-        <Navbar navLinkPromise={navLinkPromise}></Navbar>
-      </Suspense>
+      <Navbar ></Navbar>
     </>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,24 @@
-import React, { use } from "react";
+import React, { use, useEffect, useState } from "react";
 import currencyImg from "../../assets/Currency.png";
 import logo from "../../assets/logo.png";
 import { Menu } from "lucide-react";
 import "./Navbar.css";
 
-const Navbar = ({ navLinkPromise }) => {
-  const navLinkData = use(navLinkPromise);
-  console.log(navLinkData);
+const Navbar = () => {
+  const [link, setLink] = useState([]);
+
+  useEffect(() => {
+    fetch("/navbarLinkData.json")
+      .then((res) => res.json())
+      .then((data) => setLink(data));
+  }, []);
 
   return (
     <nav className="nav-container container">
       <div className="flex items-center">
         <span>
           <Menu></Menu>
+          {link.length}
         </span>
         <img className="w-11" src={logo} alt="logo" />
       </div>
